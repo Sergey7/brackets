89942aedd6c7124cbd98fba73081e5b0187eea4c
Sergey <suvoo@mail.ru>
2021-07-25 12:48:25 +0300

    feat: the logic of the function is implemented

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let tmp = '';
+  let ans = str;
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    if (bracketsConfig[i][0].includes('(') || bracketsConfig[i][0].includes('[') || bracketsConfig[i][0].includes('{') || bracketsConfig[i][0].includes('|')) {
+      tmp = tmp + `\\${bracketsConfig[i][0]}\\${bracketsConfig[i][1]}|`;
+    } else {
+      tmp = tmp + `${bracketsConfig[i][0]}${bracketsConfig[i][1]}|`;
+    }
+  }
+  let reg = RegExp(tmp, "g");
+  let checkStr = str;
+  while (true) {
+    ans = ans.replace(reg, '')
+    if (ans === checkStr) {
+      break
+    } else {
+      checkStr = ans;
+    }
+  }
+  return (ans)?false:true
+
 }
